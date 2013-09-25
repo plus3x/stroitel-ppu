b143d6ae3b92f8fa3644497f3dc64ea256e9258b
Vladislav Petrov <electronicchest@gmail.com>
2013-09-25 19:35:22 +0400

    Make main index to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 StroitelPpu::Application.routes.draw do
-  get "main/index"
+  
+  root to: 'main#index', as: 'main'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
