8eb5005a710610a4e53bdcecb8b9b7d822950f4c
Vladislav Petrov <electronicchest@gmail.com>
2013-09-29 09:13:30 +0400

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130925184404) do
+ActiveRecord::Schema.define(version: 20130928050340) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20130925184404) do
     t.string   "title"
     t.text     "description"
     t.string   "picture_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
