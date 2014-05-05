c38f4527165a08195c7df0410b99874bc3b565a0
plus3x <electronicchest@gmail.com>
2014-05-05 23:17:11 +0400

    Split layout to partials

--- app/models/role/manager.rb
+++ app/models/role/manager.rb
@@ -1,7 +1,8 @@
 class Role::Manager < User
   def can?(action, controller)
     @controller, @action = controller, action
-
-    can_do? controllers: [:sessions, :services, :type_of_products, :products, :orders]
+	
+	can_do? controllers: [:static_pages], actions: [:about, :contacts, :production, :main] or
+    can_do? controllers: [:sessions, :services, :type_of_products, :products, :orders, :debtors]
   end
 end
